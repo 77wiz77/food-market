ea66a40147f52d6a44e3f414ca61390dab1cbbd5
77wiz77 <47847427+77wiz77@users.noreply.github.com>
2021-07-18 22:53:56 +1100

    reviews

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -2,15 +2,24 @@
 document.getElementById("menu-burger").onclick = function(){
   open()
 };
-
 function open(){
   // document.getElementById("menu-burger").classList.toggle("open-menu");
   // document.getElementById("header-menu").classList.toggle("open-menu");
   // document.body.classList.toggle("fixed-page");
-
   document.getElementsByClassName("menu-burger")[0].classList.toggle("open-menu");
   document.getElementsByClassName("header-menu")[0].classList.toggle("open-menu");
   document.body.classList.toggle("fixed-page");
+}
+
+document.getElementById("header-menu").onclick = function(){
+  close()
+};
+function close(){
+  if(document.getElementsByClassName("menu-burger open-menu")[0]&&document.getElementsByClassName("header-menu open-menu")[0]){
+    document.getElementsByClassName("menu-burger open-menu")[0].classList.remove("open-menu");
+    document.getElementsByClassName("header-menu open-menu")[0].classList.remove("open-menu");
+    document.body.classList.toggle("fixed-page");
+  }
 }
 
 //Якорные ссылки на чистом JS
