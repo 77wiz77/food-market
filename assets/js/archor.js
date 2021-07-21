0a0c87bd17264f4f679e5ead31aa7ab4e812029d
77wiz77 <47847427+77wiz77@users.noreply.github.com>
2021-07-21 17:08:39 +1100

    New archor

--- assets/js/archor.js
+++ assets/js/archor.js
@@ -1,44 +1,22 @@
-// // Якорные ссылки на чистом JS
-// const anchors = document.querySelectorAll('a[href*="#"]')
+document.querySelectorAll('a[href^="#"').forEach(link => {
 
-// for (let anchor of anchors) {
-//   anchor.addEventListener('click', function (e) { // каждому якорю присваиваем обработчик события
-//     e.preventDefault() // убираем стандартное поведение
-//     const BlockID = anchor.getAttribute('href').split('#');
-//     const BlockID1 = anchor.getAttribute('href').substr(1);
-//     console.log("block 0 = " + BlockID[0] + "block 1 = " + BlockID[1]);
-//     if(BlockID[0] !== ""){
-//       // document.location.replace = BlockID[0];
-//     }
-//     document.getElementById(BlockID[1]).scrollIntoView({ //
-//     behavior: 'smooth',
-//     block: 'start'
-//     })
-//   })
-// }
+  link.addEventListener('click', function(e) {
+      e.preventDefault();
 
-var scrollAnim;
-document.querySelectorAll('a[href^="#"]').forEach(function(el){
-el.addEventListener("click", function(event) {
-  var target = document.querySelector(this.getAttribute("href"));
-  if (target != null) {
-    event.preventDefault();
-  var scrollMaxHeight = Math.max( document.body.scrollHeight, document.body.offsetHeight,document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );
-    var scroll = target.offsetTop;
-    clearTimeout(scrollAnim);
-    var animTime = 500;//ms
-    var step = (scroll - window.scrollY)/60 / (animTime/1000);
-    if(scrollMaxHeight - scroll <= window.innerHeight)
-    	scroll = scrollMaxHeight - window.innerHeight;      
-    (function(){    
-      if(Math.abs(window.scrollY - scroll) <= Math.abs(step)){       
-      	window.scrollTo( 0, scroll);
-      	return;
-      }
-      window.scrollTo( 0,parseFloat(window.scrollY) + step);
-      scrollAnim = setTimeout(arguments.callee,1000/60);
-    })();
-  }
+      let href = this.getAttribute('href').substring(1);
+
+      const scrollTarget = document.getElementById(href);
+
+      // const topOffset = document.querySelector('.scrollto').offsetHeight;
+      const topOffset = 0; // если не нужен отступ сверху 
+      const elementPosition = scrollTarget.getBoundingClientRect().top;
+      const offsetPosition = elementPosition - topOffset;
+
+      window.scrollBy({
+          top: offsetPosition,
+          behavior: 'smooth'
+      });
+  });
 });
-});
+
 
--- assets/js/menu.js
+++ assets/js/menu.js
@@ -19,15 +19,3 @@ function close(){
     document.body.classList.toggle("fixed-page");
   }
 }
-
-/*открытие и закрытие карточки*/
-
-// $(document).on('click', '[data-toggle="modal"]', function () {
-//   var target = $(this).attr('data-target');
-//   $(target).addClass('show');
-//   return false;
-// });
-// $('.modal .close').on('click', function () {
-//   $(this).closest('.modal').removeClass('show');
-//   return false;
-// });
